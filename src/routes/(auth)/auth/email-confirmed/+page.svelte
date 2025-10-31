ac48f0425efe6143a9d23806e18871687065d2a0
jade-tam <jade.tam.original@gmail.com>
2025-10-31 14:33:04 +0700

    feat: add email confirmed page

---
+++ src/routes/(auth)/auth/email-confirmed/+page.svelte
@@ -0,0 +1,13 @@
+<main class="w-screen bg-base-200">
+	<div class="mx-auto flex min-h-screen max-w-md items-center p-2">
+		<!-- HERE IS THE FORM -->
+		<div class="h-fit w-full rounded-box border bg-base-100 p-4 md:p-8">
+			<h1 class="mb-8 text-center font-serif text-3xl">Email Confirmed</h1>
+			<div class="px-32">
+				<img src="/icons/email-confirmed.svg" alt="email confirmed" />
+			</div>
+			<p class="my-2 mt-6">Your email has been confirmed, you can now login.</p>
+			<a href="/auth/login" class="btn mt-2 w-full btn-primary">Return to login</a>
+		</div>
+	</div>
+</main>
