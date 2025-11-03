bb8924411808dde2bc2c8a4cf7e579456ef53d46
jade-tam <jade.tam.original@gmail.com>
2025-11-03 20:57:44 +0700

    fix: correct new products api

--- src/routes/(main)/shopping/+page.server.ts
+++ src/routes/(main)/shopping/+page.server.ts
@@ -1,10 +1,20 @@
+import type { Category } from '$lib/types/Category';
+import type { PaginatedResult } from '$lib/types/PaginatedResult';
 import type { Product } from '$lib/types/Product';
 import { fetchApi, getSafeResult } from '$lib/utils/externalApi';
 
 export async function load() {
-	const products = getSafeResult<Product[]>(fetchApi<Product[]>('/api/home/products', 'GET'), []);
+	const queryResult = getSafeResult(
+		fetchApi<PaginatedResult<Product>>(`/api/home/products`, 'GET'),
+		{ totalItems: 0, page: 1, pageSize: 12, totalPages: 0, items: [] } as PaginatedResult<Product>
+	);
 
+	const categories = getSafeResult(
+		fetchApi<Category[]>('/api/productcategory', 'GET'),
+		[] as Category[]
+	);
 	return {
-		products
+		queryResult: queryResult,
+		categories: categories
 	};
 }
--- src/routes/(main)/shopping/+page.svelte
+++ src/routes/(main)/shopping/+page.svelte
@@ -1,5 +1,8 @@
 <script>
 	import ProductCard from '$lib/components/ui/card/ProductCard.svelte';
+	import EmptyPlaceholder from '$lib/components/ui/EmptyPlaceholder.svelte';
+	import Pagination from '$lib/components/ui/Pagination.svelte';
+	import ProductCardSkeleton from '$lib/components/ui/skeleton/ProductCardSkeleton.svelte';
 
 	const { data } = $props();
 </script>
@@ -7,15 +10,30 @@
 <section class="mt-8 flex min-h-screen flex-col items-center max-md:mt-32">
 	<h2 class="text-header1">Discover our shopping products</h2>
 
-	<div class="mt-4 grid w-full grid-cols-4 items-stretch gap-4 py-2 max-md:grid-cols-2">
-		{#await data.products}
-			{#each Array(16) as skeleton}
-				<div class="h-64 w-full skeleton"></div>
+	<div
+		class="grid w-full grid-cols-4 items-stretch gap-4 max-lg:grid-cols-3 max-md:grid-cols-2"
+	>
+		{#await data.queryResult}
+			{#each Array(12) as skeleton}
+				<ProductCardSkeleton />
 			{/each}
-		{:then products}
-			{#each products as product (product.id)}
-				<ProductCard {product} />
-			{/each}
+		{:then result}
+			{#if result.items.length}
+				{#each result.items as product (product.id)}
+					<ProductCard {product} />
+				{/each}
+
+				<div class="col-span-full">
+					<Pagination
+						page={result.page}
+						pageSize={result.pageSize}
+						totalItems={result.totalItems}
+						totalPages={result.totalPages}
+					/>
+				</div>
+			{:else}
+				<EmptyPlaceholder text="No product found" />
+			{/if}
 		{:catch err}
 			<p class="text-error">Error loading products</p>
 		{/await}
