48df073edddebc7f4804e77a95e09fe1b4c32897
jade-tam <jade.tam.original@gmail.com>
2025-12-10 06:45:51 +0700

    feat: animate login and register page

--- src/routes/(auth)/auth/login/+page.svelte
+++ src/routes/(auth)/auth/login/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
+	import AnimatedDiv from '$lib/components/animate/AnimatedDiv.svelte';
 	import FormInput from '$lib/components/ui/FormInput.svelte';
 	import { loginSchema, type LoginSchema } from '$lib/schemas/loginSchema.js';
 	import showToast, { type ToastData } from '$lib/utils/showToast';
@@ -29,45 +30,42 @@
 <main class="w-screen bg-base-200">
 	<div class="mx-auto flex min-h-screen max-w-md items-center p-2">
 		<!-- FORM ĐĂNG NHẬP -->
-		<form method="POST" class="h-fit w-full rounded-box border bg-base-100 p-4 md:p-8" use:enhance>
-			<h1 class="mb-4 text-center">Đăng nhập</h1>
-			<p class="text-header2">Chào mừng trở lại!</p>
-			<p class="mb-4 text-sm">Đăng nhập để tiếp tục cuộc hành trình của bạn.</p>
-
-			<FormInput
-				name="username"
-				label="Tên đăng nhập"
-				placeholder="Nhập tên đăng nhập của bạn..."
-				type="text"
-				icon="icon-[mingcute--user-4-fill] text-primary-content"
-				superForm={form}
-				{errors}
-			/>
-
-			<FormInput
-				name="password"
-				label="Mật khẩu"
-				placeholder="Nhập mật khẩu bí mật..."
-				type="password"
-				icon="icon-[mingcute--asterisk-fill] text-primary-content"
-				superForm={form}
-				{errors}
-			/>
-
-			<a class="float-end py-2 pl-2 text-xs font-bold" href="/auth/forgot-password"
-				>Quên mật khẩu?</a
-			>
-
-			<button class="btn mt-2 w-full btn-primary" disabled={$submitting}
-				>Đăng nhập
-				{#if $delayed}
-					<span class="loading loading-md loading-dots"></span>
-				{/if}
-			</button>
-
-			<a class="btn mt-6 w-full btn-ghost btn-sm" href="/auth/register"
-				>Bạn là người dùng mới? <strong>Đăng ký tài khoản</strong>.</a
-			>
-		</form>
+		<AnimatedDiv animateVars={{ translateY: 16 }}>
+			<form method="POST" class="h-fit w-full rounded-box border bg-base-100 p-4 md:p-8" use:enhance>
+				<h1 class="mb-4 text-center">Đăng nhập</h1>
+				<p class="text-header2">Chào mừng trở lại!</p>
+				<p class="mb-4 text-sm">Đăng nhập để tiếp tục cuộc hành trình của bạn.</p>
+				<FormInput
+					name="username"
+					label="Tên đăng nhập"
+					placeholder="Nhập tên đăng nhập của bạn..."
+					type="text"
+					icon="icon-[mingcute--user-4-fill] text-primary-content"
+					superForm={form}
+					{errors}
+				/>
+				<FormInput
+					name="password"
+					label="Mật khẩu"
+					placeholder="Nhập mật khẩu bí mật..."
+					type="password"
+					icon="icon-[mingcute--asterisk-fill] text-primary-content"
+					superForm={form}
+					{errors}
+				/>
+				<a class="float-end py-2 pl-2 text-xs font-bold" href="/auth/forgot-password"
+					>Quên mật khẩu?</a
+				>
+				<button class="btn mt-2 w-full btn-primary" disabled={$submitting}
+					>Đăng nhập
+					{#if $delayed}
+						<span class="loading loading-md loading-dots"></span>
+					{/if}
+				</button>
+				<a class="btn mt-6 w-full btn-ghost btn-sm" href="/auth/register"
+					>Bạn là người dùng mới? <strong>Đăng ký tài khoản</strong>.</a
+				>
+			</form>
+		</AnimatedDiv>
 	</div>
 </main>
--- src/routes/(auth)/auth/register/+page.svelte
+++ src/routes/(auth)/auth/register/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import AnimatedDiv from '$lib/components/animate/AnimatedDiv.svelte';
 	import FormInput from '$lib/components/ui/FormInput.svelte';
 	import { registerSchema, type RegisterSchema } from '$lib/schemas/registerSchema.js';
 	import showToast, { type ToastData } from '$lib/utils/showToast.js';
@@ -23,109 +24,115 @@
 <main class="w-screen bg-base-200">
 	<div class="mx-auto flex min-h-screen max-w-4xl items-center p-2">
 		<!-- FORM ĐĂNG KÝ -->
-		<form method="POST" class="h-fit w-full rounded-box border bg-base-100 p-4 md:p-8" use:enhance>
-			<h1 class="">Tạo tài khoản mới.</h1>
-			<p class="mb-6 text-sm">
-				Đăng ký tài khoản mới để bắt đầu hành trình khám phá các sản phẩm, khóa học và kết nối với
-				cộng đồng trên Lecom.
-			</p>
-
-			<div class="flex max-md:flex-col">
-				<!-- TRÁI -->
-				<div class="flex-1">
-					<h2 class="">Thông tin cá nhân</h2>
-
-					<FormInput
-						name="fullName"
-						label="Họ và tên"
-						placeholder="Nhập họ và tên của bạn..."
-						type="text"
-						superForm={form}
-						{errors}
-					/>
-
-					<FormInput
-						name="dateOfBirth"
-						label="Ngày sinh"
-						placeholder="Nhập ngày sinh của bạn..."
-						type="date"
-						superForm={form}
-						{errors}
-					/>
-
-					<FormInput
-						name="phoneNumber"
-						label="Số điện thoại"
-						placeholder="Nhập số điện thoại của bạn..."
-						type="tel"
-						superForm={form}
-						{errors}
-					/>
-
-					<FormInput
-						name="address"
-						label="Địa chỉ"
-						placeholder="Nhập địa chỉ của bạn..."
-						type="text"
-						superForm={form}
-						{errors}
-					/>
-				</div>
-
-				<div class="divider divider-horizontal max-md:divider-vertical"></div>
-
-				<!-- PHẢI -->
-				<div class="flex-1">
-					<h2 class="">Thông tin đăng nhập</h2>
-
-					<FormInput
-						name="userName"
-						label="Tên đăng nhập"
-						placeholder="Nhập tên đăng nhập..."
-						type="text"
-						superForm={form}
-						{errors}
-					/>
-
-					<FormInput
-						name="email"
-						label="Email"
-						placeholder="Nhập email của bạn..."
-						type="text"
-						superForm={form}
-						{errors}
-					/>
-
-					<FormInput
-						name="password"
-						label="Mật khẩu"
-						placeholder="Nhập mật khẩu bí mật..."
-						type="password"
-						superForm={form}
-						{errors}
-					/>
-
-					<FormInput
-						name="confirmPassword"
-						label="Xác nhận mật khẩu"
-						placeholder="Nhập lại mật khẩu bí mật..."
-						type="password"
-						superForm={form}
-						{errors}
-					/>
-				</div>
-			</div>
-
-			<button class="btn mt-10 w-full btn-primary" disabled={$submitting}>
-				Đăng ký tài khoản mới
-				{#if $delayed}
-					<span class="loading loading-md loading-dots"></span>
-				{/if}
-			</button>
-
-			<a class="btn mt-6 w-full btn-ghost btn-sm" href="/auth/login"
-				>Đã có tài khoản? <strong>Đăng nhập ngay</strong>.</a
+		<AnimatedDiv animateVars={{ translateY: 16 }}>
+			<form
+				method="POST"
+				class="h-fit w-full rounded-box border bg-base-100 p-4 md:p-8"
+				use:enhance
 			>
-		</form>
+				<h1 class="">Tạo tài khoản mới.</h1>
+				<p class="mb-6 text-sm">
+					Đăng ký tài khoản mới để bắt đầu hành trình khám phá các sản phẩm, khóa học và kết nối với
+					cộng đồng trên Lecom.
+				</p>
+
+				<div class="flex max-md:flex-col">
+					<!-- TRÁI -->
+					<div class="flex-1">
+						<h2 class="">Thông tin cá nhân</h2>
+
+						<FormInput
+							name="fullName"
+							label="Họ và tên"
+							placeholder="Nhập họ và tên của bạn..."
+							type="text"
+							superForm={form}
+							{errors}
+						/>
+
+						<FormInput
+							name="dateOfBirth"
+							label="Ngày sinh"
+							placeholder="Nhập ngày sinh của bạn..."
+							type="date"
+							superForm={form}
+							{errors}
+						/>
+
+						<FormInput
+							name="phoneNumber"
+							label="Số điện thoại"
+							placeholder="Nhập số điện thoại của bạn..."
+							type="tel"
+							superForm={form}
+							{errors}
+						/>
+
+						<FormInput
+							name="address"
+							label="Địa chỉ"
+							placeholder="Nhập địa chỉ của bạn..."
+							type="text"
+							superForm={form}
+							{errors}
+						/>
+					</div>
+
+					<div class="divider divider-horizontal max-md:divider-vertical"></div>
+
+					<!-- PHẢI -->
+					<div class="flex-1">
+						<h2 class="">Thông tin đăng nhập</h2>
+
+						<FormInput
+							name="userName"
+							label="Tên đăng nhập"
+							placeholder="Nhập tên đăng nhập..."
+							type="text"
+							superForm={form}
+							{errors}
+						/>
+
+						<FormInput
+							name="email"
+							label="Email"
+							placeholder="Nhập email của bạn..."
+							type="text"
+							superForm={form}
+							{errors}
+						/>
+
+						<FormInput
+							name="password"
+							label="Mật khẩu"
+							placeholder="Nhập mật khẩu bí mật..."
+							type="password"
+							superForm={form}
+							{errors}
+						/>
+
+						<FormInput
+							name="confirmPassword"
+							label="Xác nhận mật khẩu"
+							placeholder="Nhập lại mật khẩu bí mật..."
+							type="password"
+							superForm={form}
+							{errors}
+						/>
+					</div>
+				</div>
+
+				<button class="btn mt-10 w-full btn-primary" disabled={$submitting}>
+					Đăng ký tài khoản mới
+					{#if $delayed}
+						<span class="loading loading-md loading-dots"></span>
+					{/if}
+				</button>
+
+				<a class="btn mt-6 w-full btn-ghost btn-sm" href="/auth/login"
+					>Đã có tài khoản? <strong>Đăng nhập ngay</strong>.</a
+				>
+			</form>
+		</AnimatedDiv>
 	</div>
 </main>
