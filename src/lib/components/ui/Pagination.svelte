480e7c0f8d2cafc24595088eb5d1f6b25d5132b7
jade-tam <jade.tam.original@gmail.com>
2025-12-11 07:39:55 +0700

    feat: improve course search page

--- src/lib/components/ui/Pagination.svelte
+++ src/lib/components/ui/Pagination.svelte
@@ -46,7 +46,7 @@
 
 <div class="flex items-center justify-between gap-4">
 	<p class="text-sm opacity-60">
-		Đang hiển thị sản phẩm <strong>{startItemNumber}</strong> - <strong>{endItemNumber}</strong>
+		Đang hiển thị <strong>{startItemNumber}</strong> - <strong>{endItemNumber}</strong>
 		trên tổng số <strong>{totalItems}</strong>
 	</p>
 
--- src/routes/(main)/learning/courses/+page.svelte
+++ src/routes/(main)/learning/courses/+page.svelte
@@ -5,7 +5,7 @@
 	import EmptyPlaceholder from '$lib/components/ui/EmptyPlaceholder.svelte';
 	import Pagination from '$lib/components/ui/Pagination.svelte';
 	import RadioInput from '$lib/components/ui/RadioInput.svelte';
-	import CourseCardSkeleton from '$lib/components/ui/skeleton/CourseCardSkeleton.svelte';
+	import LoadingPlaceholder from '$lib/components/ui/skeleton/LoadingPlaceholder.svelte';
 
 	const { data } = $props();
 
@@ -52,21 +52,21 @@
 			<div class="loading loading-infinity"></div>
 		{:then queryResult}
 			<p>
-				Showing <strong>{queryResult.items.length} results</strong>
+				Hiển thị <strong>{queryResult.items.length} kết quả</strong>
 				{#if search}
-					for <span class="font-bold">
+					cho <span class="font-bold">
 						'{search}'
 					</span>
 				{/if}
 			</p>
 		{:catch err}
-			<p class="text-error">Error loading courses</p>
+			<p class="text-error">Lỗi khi tải khóa học</p>
 		{/await}
 	</div>
 
-	<div class="grid w-full grid-cols-12 gap-6 max-md:grid-cols-1">
+	<div class="grid w-full grid-cols-12 gap-2 max-md:grid-cols-1">
 		<div class="col-span-3 h-fit rounded-box border bg-base-100 p-4">
-			<p class="font-serif text-lg font-bold">Filter by category</p>
+			<p class="font-serif text-lg font-bold">Lọc theo danh mục</p>
 			<div class="mt-2 flex flex-col gap-2">
 				{#await data.categories}
 					<div class="loading loading-infinity"></div>
@@ -80,25 +80,23 @@
 							}))}
 						/>
 					{:else}
-						<p class="text-xs text-base-content/60 italic">No category found</p>
+						<p class="text-xs text-base-content/60 italic">Không tìm thấy danh mục nào</p>
 					{/if}
 				{:catch err}
-					<p class="text-error">Error loading courses</p>
+					<p class="text-error">Lỗi khi tải danh mục khóa học</p>
 				{/await}
 
 				<!-- =================================== -->
 				<div class="divider mb-1"></div>
-				<button class="btn btn-primary" onclick={handleApplyFilter}>Apply</button>
-				<button class="btn" onclick={handleClearAllFilter}>Clear All</button>
+				<button class="btn btn-primary" onclick={handleApplyFilter}>Áp dụng</button>
+				<button class="btn" onclick={handleClearAllFilter}>Xóa tất cả</button>
 			</div>
 		</div>
 		<div
-			class="col-span-9 grid h-fit w-full grid-cols-4 gap-4 max-lg:grid-cols-3 max-md:grid-cols-2"
+			class="col-span-9 grid h-fit w-full grid-cols-3 gap-y-6 gap-x-2 max-lg:grid-cols-2 max-md:grid-cols-2"
 		>
 			{#await data.queryResult}
-				{#each Array(4) as skeleton}
-					<CourseCardSkeleton />
-				{/each}
+				<LoadingPlaceholder text="Đang tải danh sách khóa học..." />>
 			{:then result}
 				{#if result.items.length}
 					{#each result.items as course (course.id)}
@@ -114,10 +112,10 @@
 						/>
 					</div>
 				{:else}
-					<EmptyPlaceholder text="No course found" />
+					<EmptyPlaceholder text="Không tìm thấy khóa học nào" />
 				{/if}
 			{:catch err}
-				<p class="text-error">Error loading courses</p>
+				<p class="text-error">Lỗi khi tải khóa học</p>
 			{/await}
 		</div>
 	</div>
