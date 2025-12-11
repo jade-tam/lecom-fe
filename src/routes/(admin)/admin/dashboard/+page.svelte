f32caa05a693512ffb0b5d7c5ccdb4bdfb635a74
jade-tam <jade.tam.original@gmail.com>
2025-12-11 12:06:53 +0700

    feat: add a button in admin dashboard

--- src/routes/(admin)/admin/dashboard/+page.svelte
+++ src/routes/(admin)/admin/dashboard/+page.svelte
@@ -350,6 +350,7 @@
 							>{formatVND(dashboardData.overview.platformFee)}</strong
 						>
 					</div>
+					<a class="btn btn-sm mt-2 btn-success" href={resolve('/(admin)/admin/platform-wallet')}><span class="icon-[mingcute--wallet-4-fill] text-lg"></span>Xem chi tiết ví nền tảng</a>
 				</AnimatedDiv>
 				<AnimatedDiv
 					animateVars={{ translateY: 24, delay: 0.4 }}
