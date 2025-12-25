5a1ab32255fb216fa5fea86caaa807bae6d95eb0
jade-tam <jade.tam.original@gmail.com>
2025-12-25 21:12:38 +0700

    feat: translate forget password page

--- src/routes/(auth)/auth/forgot-password/+page.svelte
+++ src/routes/(auth)/auth/forgot-password/+page.svelte
@@ -27,28 +27,28 @@
 	<div class="mx-auto flex min-h-screen max-w-md items-center p-2">
 		<!-- HERE IS THE FORM -->
 		<form method="POST" class="h-fit w-full rounded-box border bg-base-100 p-4 md:p-8" use:enhance>
-			<h1 class="mb-8 text-center font-serif text-3xl">Recover your password</h1>
+			<h1 class="mb-8 text-center font-serif text-3xl">Đặt lại mật khẩu</h1>
 
 			<FormInput
 				name="email"
-				label="What is your email?"
-				placeholder="Email of your account..."
+				label="Email của bạn là gì?"
+				placeholder="Nhập email liên kết với tài khoản..."
 				type="text"
 				icon="icon-[fa7-solid--envelope]"
-				help="We will send you a verification to this email"
+				help="Chungs tôi sẽ gửi một email xác minh để đặt lại mật khẩu của bạn."
 				superForm={form}
 				{errors}
 			/>
 
 			<button class="btn mt-2 w-full btn-primary" disabled={$submitting}
-				>Send verification email
+				>Gửi email xác minh
 				{#if $delayed}
 					<span class="loading loading-md loading-dots"></span>
 				{/if}
 			</button>
 
-			<a class="btn mt-6 w-full btn-ghost btn-sm" href="/auth/register"
-				>Suddenly remember your password?<strong>Back to login</strong>.</a
+			<a class="btn mt-6 w-full btn-ghost btn-sm" href="/auth/login"
+				>Đã nhớ mật khẩu?<strong>Quay lại đăng nhập</strong>.</a
 			>
 		</form>
 	</div>
